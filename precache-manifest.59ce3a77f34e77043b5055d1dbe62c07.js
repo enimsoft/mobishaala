5c4c14f4076d70f780c99ea12dcfbe5fa5434234
pranavdhoolia <dhoolia.pranav@gmail.com>
2021-08-20 11:02:53 +0530

    remove build from gitignore

---
+++ precache-manifest.59ce3a77f34e77043b5055d1dbe62c07.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16cac8e7044dedf3c2cad34a4839822a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ffa9f1b3827e17ca9d4",
+    "url": "/static/css/main.3bfa81bf.chunk.css"
+  },
+  {
+    "revision": "12cbe4915ff4e758f89a",
+    "url": "/static/js/2.95610910.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.95610910.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ffa9f1b3827e17ca9d4",
+    "url": "/static/js/main.72ed0ea3.chunk.js"
+  },
+  {
+    "revision": "081e3a871795653bb327",
+    "url": "/static/js/runtime-main.4b6fb5b4.js"
+  },
+  {
+    "revision": "90509a102a6ad920814a91a89e288e1b",
+    "url": "/static/media/1.90509a10.svg"
+  },
+  {
+    "revision": "03d12227cae33ef9e8673b92031a11ee",
+    "url": "/static/media/2.03d12227.svg"
+  },
+  {
+    "revision": "5ab7e2bab7aae8ea17b75e36aaad3ed1",
+    "url": "/static/media/3.5ab7e2ba.svg"
+  },
+  {
+    "revision": "c686e46fc2558a699716ae24bbacce48",
+    "url": "/static/media/4.c686e46f.svg"
+  },
+  {
+    "revision": "b249b5e48e74e53a33235cc46d345fed",
+    "url": "/static/media/5.b249b5e4.svg"
+  },
+  {
+    "revision": "3516304d7c37073ec734048915043f1d",
+    "url": "/static/media/background.3516304d.svg"
+  },
+  {
+    "revision": "910a2df1644877c3dcd0b0b04525a883",
+    "url": "/static/media/background.910a2df1.svg"
+  },
+  {
+    "revision": "be7724a2fd994654f1a245d02b425b96",
+    "url": "/static/media/background.be7724a2.svg"
+  },
+  {
+    "revision": "62f7df92fe3c155df1c9a25677ffcc0c",
+    "url": "/static/media/backgroundLayer.62f7df92.svg"
+  },
+  {
+    "revision": "67a446da4c68ea6c456feaa477aa7186",
+    "url": "/static/media/btnScheduleCall.67a446da.svg"
+  },
+  {
+    "revision": "a8fd5001274ca62f3c5287119cf03e00",
+    "url": "/static/media/card1.a8fd5001.svg"
+  },
+  {
+    "revision": "1949979524dad3eec2247db4fadcecee",
+    "url": "/static/media/card2.19499795.svg"
+  },
+  {
+    "revision": "85a588a2143ef5399169aea8bd39a72a",
+    "url": "/static/media/card3.85a588a2.svg"
+  },
+  {
+    "revision": "f759757c68d4fcd02d70054c4f5ad68d",
+    "url": "/static/media/card4.f759757c.svg"
+  },
+  {
+    "revision": "a8fd5001274ca62f3c5287119cf03e00",
+    "url": "/static/media/card5.a8fd5001.svg"
+  },
+  {
+    "revision": "8c6d01a85cd7b2dd54e611b31798b8ae",
+    "url": "/static/media/customfooter.8c6d01a8.svg"
+  },
+  {
+    "revision": "fd475e5fadfbfe6b8a8bdd3af7425c6e",
+    "url": "/static/media/img1.fd475e5f.svg"
+  },
+  {
+    "revision": "9bd92fa1224495f05aa57a00cd713523",
+    "url": "/static/media/img2.9bd92fa1.svg"
+  },
+  {
+    "revision": "dfd9e417250f74110ab72c041fe1b690",
+    "url": "/static/media/img3.dfd9e417.svg"
+  },
+  {
+    "revision": "32940d5ce831e78f5a009291382cc1a0",
+    "url": "/static/media/justTheBeginning.32940d5c.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cea808846472a0481f8bec20455caa3e",
+    "url": "/static/media/logo.cea80884.svg"
+  },
+  {
+    "revision": "b37d77d274c9c2f4d44a347481370faa",
+    "url": "/static/media/man.b37d77d2.svg"
+  },
+  {
+    "revision": "5903d2ea6b14150197f2b886d8256b74",
+    "url": "/static/media/moneyBack90days.5903d2ea.svg"
+  },
+  {
+    "revision": "91a90c7b323d634376804ccff366c131",
+    "url": "/static/media/people.91a90c7b.svg"
+  },
+  {
+    "revision": "49d37034d28fb352c8567d174f59cf37",
+    "url": "/static/media/transformYourBusiness.49d37034.svg"
+  }
+]);
